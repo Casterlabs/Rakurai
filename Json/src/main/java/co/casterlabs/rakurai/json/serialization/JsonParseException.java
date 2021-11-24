75e85bef6de2322e7116fd3ee122d9e603ffd206
Ender <33337309+e3ndr@users.noreply.github.com>
2021-11-24 12:31:25 -0600

    Json: Make JsonParseException extend IOException.

--- Json/src/main/java/co/casterlabs/rakurai/json/serialization/JsonParseException.java
+++ Json/src/main/java/co/casterlabs/rakurai/json/serialization/JsonParseException.java
@@ -1,16 +1,22 @@
 package co.casterlabs.rakurai.json.serialization;
 
+import java.io.IOException;
+
 import lombok.NonNull;
 
-public class JsonParseException extends Exception {
+public class JsonParseException extends IOException {
     private static final long serialVersionUID = 898397259977630282L;
 
     public JsonParseException(@NonNull String reason) {
         super(reason);
     }
 
-    public JsonParseException(@NonNull Exception e) {
-        super(e);
+    public JsonParseException(@NonNull String reason, @NonNull Exception cause) {
+        super(reason, cause);
+    }
+
+    public JsonParseException(@NonNull Exception cause) {
+        super(cause);
     }
 
 }
