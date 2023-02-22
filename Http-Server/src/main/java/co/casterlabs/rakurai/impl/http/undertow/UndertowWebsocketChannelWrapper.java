c1293a0b8f79b3d896b77c52149f62bda3b7ba41
Ender <33337309+e3ndr@users.noreply.github.com>
2023-02-21 20:51:40 -0600

    Http-Server: Add some additional logging.

--- Http-Server/src/main/java/co/casterlabs/rakurai/impl/http/undertow/UndertowWebsocketChannelWrapper.java
+++ Http-Server/src/main/java/co/casterlabs/rakurai/impl/http/undertow/UndertowWebsocketChannelWrapper.java
@@ -3,6 +3,7 @@ package co.casterlabs.rakurai.impl.http.undertow;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
+import co.casterlabs.rakurai.StringUtil;
 import co.casterlabs.rakurai.io.http.websocket.Websocket;
 import co.casterlabs.rakurai.io.http.websocket.WebsocketCloseCode;
 import co.casterlabs.rakurai.io.http.websocket.WebsocketSession;
@@ -12,19 +13,27 @@ import lombok.NonNull;
 
 public class UndertowWebsocketChannelWrapper extends Websocket {
     private WebSocketChannel channel;
+    private boolean logWebsocketFrames;
 
     public UndertowWebsocketChannelWrapper(WebSocketChannel channel, WebsocketSession session) {
         super(session);
         this.channel = channel;
+        this.logWebsocketFrames = System.getProperty("rakurailogwebsocketframes", "").equals("true");
     }
 
     @Override
     public void send(@NonNull String message) throws IOException {
+        if (this.logWebsocketFrames) {
+            this.getSession().getLogger().trace("Sending text: %s", message);
+        }
         WebSockets.sendText(message, this.channel, null);
     }
 
     @Override
     public void send(@NonNull byte[] bytes) throws IOException {
+        if (this.logWebsocketFrames) {
+            this.getSession().getLogger().trace("Sending bytes: %s", StringUtil.bytesToHex(bytes));
+        }
         WebSockets.sendBinary(ByteBuffer.wrap(bytes), this.channel, null);
     }
 
