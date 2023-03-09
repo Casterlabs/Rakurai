fa6f8e43a9382972e2b28c21b8b89b5b858adcce
Ender <33337309+e3ndr@users.noreply.github.com>
2023-03-08 18:10:35 -0600

    Clean up the Http-Server impls and where they're located.

---
+++ Http-Server/ImplNano/src/main/java/co/casterlabs/rakurai/http/server/impl/nano/NanoHttpServerBuilder.java
@@ -0,0 +1,48 @@
+package co.casterlabs.rakurai.http.server.impl.nano;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLServerSocketFactory;
+
+import co.casterlabs.rakurai.io.http.server.HttpListener;
+import co.casterlabs.rakurai.io.http.server.HttpServer;
+import co.casterlabs.rakurai.io.http.server.HttpServerBuilder;
+import co.casterlabs.rakurai.io.http.server.HttpServerImplementation;
+import fi.iki.elonen.NanoHTTPD;
+import lombok.NonNull;
+
+public class NanoHttpServerBuilder extends HttpServerBuilder {
+
+    @Override
+    public HttpServer build(@NonNull HttpListener listener) throws IOException {
+        return new NanoHttpServer(listener, this.hostname, this.port, this);
+    }
+
+    @Override
+    public HttpServer buildSecure(@NonNull HttpListener listener) throws IOException, KeyStoreException, NoSuchAlgorithmException, CertificateException, UnrecoverableKeyException {
+        this.applyDHSize();
+
+        KeyStore keystore = KeyStore.getInstance("jks");
+        keystore.load(new FileInputStream(this.ssl.getKeystoreLocation()), this.ssl.getKeystorePassword());
+
+        KeyManagerFactory managerFactory = KeyManagerFactory.getInstance("SunX509");
+        managerFactory.init(keystore, this.ssl.getKeystorePassword());
+
+        SSLServerSocketFactory factory = NanoHTTPD.makeSSLSocketFactory(keystore, managerFactory);
+
+        return new NanoHttpServer(listener, this.hostname, this.port, this, new WrappedSSLSocketFactory(factory, this.ssl), this.convertTLS());
+    }
+
+    @Override
+    public HttpServerImplementation getImplementation() {
+        return HttpServerImplementation.NANO;
+    }
+
+}
